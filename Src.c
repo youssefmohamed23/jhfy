4b55bfe90364b34b0436ba27cdb36495e49f476a
youssefmohamed23 <yossefmuhamedd@gmail.com>
2022-08-27 11:18:36 +0200

    2.0.1

--- Src.c
+++ Src.c
@@ -7,6 +7,6 @@ char is_Even (int a ){
 return a&1;
 }
 int divas (int a , int b){
-
-return a/b;
+if(b ==0) return -1;
+else return a/b;
 }
--- main.c
+++ main.c
@@ -8,9 +8,14 @@ int main()
 
 int x,y;
 x= 5;
-y=2;
+y=1;
 printf("Sum of %d , %d = %d \n",x,y,sum(x,y));
 printf("Even = %d , Odd = %d \n",!is_Even(x),is_Even(x));
-printf("Div = %d \n",divas(x,y));
+if(divas(x,y)==-1){
+    printf("Cannot divide by 0\n");
+}
+else {
+printf("Div = %d \n",divas(x,y));}
+
     return 0;
 }
